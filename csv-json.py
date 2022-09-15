b940345062a5b5a79c5b8bf7feba9fce74e4c4a4
sayk-77 <98908548+sayk-77@users.noreply.github.com>
2022-09-15 20:19:42 +0300

    Update csv-json.py

--- csv-json.py
+++ csv-json.py
@@ -1,9 +1,9 @@
 import csv
 import json
 
-with open('1991.csv') as e:
+with open('1991.csv', r, encoding='utf-8') as e:
     reader = csv.DictReader(e)
     rows = list(reader)
 
-with open('1991.json', 'w') as f:
+with open('1991.json', 'w', encoding='utf-8') as f:
     json.dump(rows, f)
